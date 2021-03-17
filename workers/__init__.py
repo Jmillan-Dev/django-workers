9c00cd3e277cd60e6948d3395ce9f475e649aef0
Josué Millán Zamora <jmillan.dev@protonmail.com>
2021-03-17 16:57:14 -0700

    Updating import in __int__.py

--- workers/__init__.py
+++ workers/__init__.py
@@ -6,5 +6,5 @@ def task(*arg, **kwarg):
     Need to wrap this task import so it doesnt explode during Django init.
 
     """
-    from workers.worker import task
+    from .worker import task
     return task(*arg, **kwarg)
